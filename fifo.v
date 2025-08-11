64805ef0212502c206c696d1ff16b5a5a8210c36
ganshyamsharma <gsmsharma99@gmail.com>
2025-08-12 00:22:26 +0530

    updated the fifo logic to include almost full and almost empty flags along with provision for clock domain crossing

--- fifo.v
+++ fifo.v
@@ -1,39 +1,56 @@
-
-module FIFO #(parameter DEPTH = 8, WIDTH = 8)(
-	input i_clk, i_rst_n,
-	input i_wr_en, i_rd_en,
-	input [WIDTH - 1 : 0] i_wr_data,
-	output reg [WIDTH - 1 : 0] o_rd_data,
-	output o_empty, o_mem_full
+module fifo_64 #(parameter DEPTH = 64, parameter WIDTH = 8)(
+	input i_clk, i_wr_clk, i_rd_clk, i_rst_n, i_wr_en, i_rd_en,
+	input [$clog2(DEPTH)-1:0] i_almost_full_lim, i_almost_empty_lim,
+	input [WIDTH-1:0] i_wr_data,
+	output o_almost_full, o_almost_empty, o_full, o_empty,
+	output [WIDTH-1:0] o_rd_data
 );
-	reg [2:0] r_wr_ptr, r_rd_ptr;
-	reg [WIDTH - 1 : 0] r_fifo_reg[DEPTH];
+	
+	reg [WIDTH-1:0] r_fifo_mem [DEPTH-1 : 0];
+	reg [$clog2(DEPTH)-1:0]r_wr_ptr = 0, r_rd_ptr = 0;
+	
+	reg [WIDTH-1:0] r_rd_data;
+	
+	assign o_rd_data = r_rd_data;
 	
 	always @(posedge i_clk) begin
 		if(~i_rst_n) begin
-			r_wr_ptr <= 3'd0;
-			r_rd_ptr <= 3'd0;
-			o_rd_data <= 0;
+			r_wr_ptr 	<= 0;
+			r_rd_ptr 	<= 0;
+			r_rd_data 	<= 0;
+		end
+		else begin
+			r_wr_ptr 	<= r_wr_ptr;
+			r_rd_ptr 	<= r_rd_ptr;
+			r_rd_data 	<= r_rd_data;
 		end
 	end
 	
-	always @(posedge i_clk) begin
-		if(i_rd_en & ~o_empty) begin
-			o_rd_data <= r_fifo_reg[r_rd_ptr];
-			r_rd_ptr <= r_rd_ptr + 1'b1;
+	always @(posedge i_wr_clk) begin
+		if(i_wr_en & ~o_full) begin
+			r_fifo_mem[r_wr_ptr] <= i_wr_data;
+			r_wr_ptr <= r_wr_ptr + 1;
+		end
+		else begin
+			r_wr_ptr <= r_wr_ptr;
 		end
 	end
 	
-	always @(posedge i_clk) begin
-		if(i_wr_en & ~o_mem_full) begin
-			r_fifo_reg[r_wr_ptr] <= i_wr_data;
-			r_wr_ptr <= r_wr_ptr + 1'b1;
+	always @(posedge i_rd_clk) begin
+		if(i_rd_en & ~i_wr_en & ~o_empty) begin
+			r_rd_data <= r_fifo_mem[r_rd_ptr];
+			r_rd_ptr <= r_rd_ptr + 1;
+		end
+		else begin
+			r_rd_ptr <= r_rd_ptr;
 		end
 	end
 	
-	assign o_empty = (r_rd_ptr == r_wr_ptr);
-	assign o_mem_full = ((r_wr_ptr+1'b1) == r_rd_ptr);
-
+	assign o_almost_full 	= ((r_wr_ptr - r_rd_ptr) < i_almost_full_lim) ? 1'b0 : 1'b1;
+	assign o_almost_empty 	= ((r_wr_ptr - r_rd_ptr) > i_almost_empty_lim) ? 1'b0 : 1'b1;
+	assign o_full 			= ((r_wr_ptr + 1'b1) == r_rd_ptr);
+	assign o_empty 			= (r_wr_ptr == r_rd_ptr);
+	
 endmodule
 			
 			
